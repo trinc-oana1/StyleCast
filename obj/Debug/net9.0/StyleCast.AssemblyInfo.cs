b2d3277be74ec240bdc74f8555c2a3bbabf1de60
lorena-maria03 <vasilemarialorena@gmail.com>
2025-12-10 00:36:58 +0200

    commit lv

--- obj/Debug/net9.0/StyleCast.AssemblyInfo.cs
+++ obj/Debug/net9.0/StyleCast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StyleCast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3250cda4337926e79df7daf6e2fd77a74c7eabf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dfc3a4bd80b48cee44440e368a133ab9179765f")]
 [assembly: System.Reflection.AssemblyProductAttribute("StyleCast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StyleCast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
