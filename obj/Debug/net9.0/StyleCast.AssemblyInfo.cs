f6b566ea1774adeb6cefe291ec1165c549280352
Oana Trinc <trinc.mi.oana@student.utcluj.ro>
2026-01-09 20:24:50 +0200

    Task 3 - offline detection for the frontend part

--- obj/Debug/net9.0/StyleCast.AssemblyInfo.cs
+++ obj/Debug/net9.0/StyleCast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StyleCast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+907f820f33298841d15eac37d0c216c23d26d127")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b14930ae70a411c35a404f1e60e15a062e6371f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("StyleCast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StyleCast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
